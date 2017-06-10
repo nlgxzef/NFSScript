0ca906345701baac360dd0aca1648108f43fd504
Dennis Stanistan <dennisstanistan@gmail.com>
2017-06-11 00:53:02 +0300

    Added GameMemory.WriteData

--- Core/GameMemory.cs
+++ Core/GameMemory.cs
@@ -1,4 +1,6 @@
-﻿namespace NFSScript.Core
+﻿using System;
+
+namespace NFSScript.Core
 {
     /// <summary>
     /// Where the magic happens.
@@ -14,5 +16,21 @@
         /// A generic memory.
         /// </summary>
         public static GMemory genericMemory;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static IntPtr WriteData(byte[] data)
+        {
+            uint size = (uint)data.Length;
+
+            IntPtr hAlloc = NativeMethods.VirtualAllocEx(memory.ProcessHandle, IntPtr.Zero, size, AllocationType.Commit, MemoryProtection.ExecuteReadWrite);
+
+            memory.WriteByteArray(hAlloc, data);
+
+            return hAlloc;
+        }
     }
 }
